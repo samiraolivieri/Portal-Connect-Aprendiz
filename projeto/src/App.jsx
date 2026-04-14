1315ddaf52db25860338f43543e684f445888823
Ismael0109 <ismaelsantanateixeira@gmail..com>
2026-04-14 15:56:48 -0300

    Ismael Commit 1404

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 import Login from './pages/Login'
-
+import Justificativas from './pages/Justificativas.jsx'
+// Importe as outras páginas que vamos criar (mesmo que vazias agora)
+// import Boletim from './pages/Boletim'; 
+import './App.css';
 
 function App() {
- 
   return (
-    <Login />
-  )
+    <div className="app-layout">
+      <Sidebar />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/boletim" element={<h1 style={{color: 'black'}}>Página de Boletim</h1>} />
+          <Route path="/justificativas" element={<Justificativas/>} />
+          <Route path="/mural" element={<h1 style={{color: 'black'}}>Mural de Avisos</h1>} />
+          <Route path="/carreiras" element={<h1 style={{color: 'black'}}>Portal Carreiras</h1>} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- projeto/src/pages/Login.jsx
+++ projeto/src/pages/Login.jsx
@@ -12,22 +12,22 @@ export default function Login() {
         email: email,
         password: senha
       });
-  
+
       // Salva os dados do usuário no localStorage
       localStorage.setItem("usuario", JSON.stringify(resposta.data.user));
-  
+
       console.log("Login realizado!", resposta.data.user);
-  
-      // Redireciona baseado no perfil
-      const perfil = resposta.data.user.perfil;
-if (perfil === "aprendiz") {
-  window.location.href = "/dashboard-aprendiz";
-} else if (perfil === "gestor") {
-  window.location.href = "/dashboard-gestor";
-} else if (perfil === "pedagogia") {
-  window.location.href = "/dashboard-pedagogia";
-}
-  
+
+      // Redireciona baseado no nivel 
+      const nivel = resposta.data.user.nivel;
+      if (nivel === "aprendiz") {
+        window.location.href = "/";
+      } else if (nivel === "gestor") {
+        window.location.href = "/dashboard-gestor";
+      } else if (nivel === "pedagogia") {
+        window.location.href = "/dashboard-pedagogia";
+      }
+
     } catch (erro) {
       if (erro.response?.status === 401) {
         alert("E-mail ou senha incorretos!");
